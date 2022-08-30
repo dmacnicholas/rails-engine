fd72e328feb4351a4ed0423526ac3ab017682441
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-30 09:44:50 -0500

    Add test for finding one merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -27,4 +27,26 @@ describe "Merchants API" do
 
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+
+    expect(merchant).to have_key(:created_at)
+    expect(merchant[:created_at]).to be_a(String)
+
+    expect(merchant).to have_key(:updated_at)
+    expect(merchant[:updated_at]).to be_a(String)
+  end
 end
