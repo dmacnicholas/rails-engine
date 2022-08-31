5fe8b19e845694ac393a65a2446811a598675850
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-30 23:18:01 -0500

    Add index and show methods

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::ItemsController < ApplicationController
-def index
+
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    if Item.exists?(params[:id])
+      item = Item.find(params[:id])
+      render json: ItemSerializer.new(item)
+    else
+      render status: 404
+  end
 end
 end
