0c0dd507b9dd44634e75a7afa6a100b896278fc0
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-01 22:31:17 -0500

    Add method for find all items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
 
+  def self.find_all_by_name(name)
+    where("name ILIKE ?", "%#{name}%")
+  end
 end
